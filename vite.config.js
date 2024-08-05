bc52109b00af59c98b9fc6b7d10f74da227575b1
Siddharth Tayal <125152950+Siddharth-Tayal@users.noreply.github.com>
2024-08-05 14:17:29 +0530

    Adding xml in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // or your framework's plugin
+import sitemap from 'vite-plugin-sitemap';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://www.example.com', // replace with your domain
+      routes: [
+        '/',
+        '/#/features',
+        '/feature/gcp',
+        '/feature/film-production',
+        '/feature/schools-colleges-function',
+        '/#/footer',
+        // Add your routes here
+      ],
+    }),
+  ],
+});
